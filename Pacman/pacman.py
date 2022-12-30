a3249893391fbbebb3105a474c246c21bca0c074
Jhogois <jhogois@live.com>
2022-12-30 12:37:38 -0300

    Pacman Labirynt

--- Pacman/pacman.py
+++ Pacman/pacman.py
@@ -1,3 +1,37 @@
 import pygame
 
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+SPEED = 0.2
+RAY = 30
 pygame.init()
+
+window = pygame.display.set_mode((640, 480), 0)
+x = 10
+y = 10
+x_speed = SPEED
+y_speed = SPEED
+
+while True:
+    # Calculate the rules
+    x = x + x_speed
+    y = y + y_speed
+
+    if x + RAY > 640:
+        x_speed = -SPEED
+    if x - RAY < 0:
+        x_speed = SPEED
+    if y + RAY > 480:
+        y_speed = -SPEED
+    if y - RAY < 0:
+        y_speed = SPEED
+
+    # Paint
+    window.fill(BLACK)
+    pygame.draw.circle(window, YELLOW, (int(x), int(y)), RAY, 0)
+    pygame.display.update()
+
+    # Events
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
---
+++ Pacman/pacman_2.py
@@ -0,0 +1,140 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600), 0)
+
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+SPEED = 1
+
+
+class Scenario:
+    def __init__(self, size) -> None:
+        self.size = size
+        self.matrix = [
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 0, 0, 0, 0, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        ]
+
+    def paint_line(self, screen, number_line, line):
+        for number_column, column in enumerate(line):
+            x = number_column * self.size
+            y = number_line * self.size
+            color = BLACK
+            if column == 2:
+                color = BLUE
+            pygame.draw.rect(screen, color, (x, y, self.size, self.size), 0)
+
+    def paint(self, screen):
+        for number_line, line in enumerate(self.matrix):
+            self.paint_line(screen, number_line, line)
+
+
+class Pacman:
+
+    def __init__(self) -> None:
+        self.column = 1
+        self.line = 1
+        self.x_center = 400
+        self.y_center = 300
+        self.size = 800 // 30
+        self.x_speed = 0
+        self.y_speed = 0
+        self.ray = self.size // 2
+
+    def calculate_rules(self):
+        self.column = self.column + self.x_speed
+        self.line = self.line + self.y_speed
+        self.x_center = int(self.column * self.size + self.ray)
+        self.y_center = int(self.line * self.size + self.ray)
+
+    def paint(self, screen):
+        # Draw the Pacman body
+        pygame.draw.circle(
+            screen, YELLOW, (self.x_center, self.y_center), self.ray
+            )
+        # Mouth drawing
+        mouth_corner = (self.x_center, self.y_center)
+        upper_lip = (self.x_center + self.ray, self.y_center - self.ray)
+        lower_lip = (self.x_center + self.ray, self.y_center)
+        points = [mouth_corner, upper_lip, lower_lip]
+        pygame.draw.polygon(screen, BLACK, points, 0)
+
+        # Pacman's eye
+        eye_x = int(self.x_center + self.ray / 3)
+        eye_y = int(self.y_center - self.ray * 0.70)
+        eye_ray = int(self.ray / 10)
+        pygame.draw.circle(screen, BLACK, (eye_x, eye_y), eye_ray, 0)
+
+    def process_events(self, events):
+        for e in events:
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_RIGHT:
+                    self.x_speed = SPEED
+                elif e.key == pygame.K_LEFT:
+                    self.x_speed = -SPEED
+                elif e.key == pygame.K_UP:
+                    self.y_speed = -SPEED
+                elif e.key == pygame.K_DOWN:
+                    self.y_speed = SPEED
+            elif e.type == pygame.KEYUP:
+                if e.key == pygame.K_RIGHT:
+                    self.x_speed = 0
+                elif e.key == pygame.K_LEFT:
+                    self.x_speed = 0
+                elif e.key == pygame.K_UP:
+                    self.y_speed = 0
+                elif e.key == pygame.K_DOWN:
+                    self.y_speed = 0
+
+
+if __name__ == "__main__":
+    pacman = Pacman()
+    scenario = Scenario(600 // 30)
+
+    while True:
+        # Calculate the rules
+        pacman.calculate_rules()
+
+        # Paint the screen
+        screen.fill(BLACK)
+        scenario.paint(screen)
+        pacman.paint(screen)
+        pygame.display.update()
+        pygame.time.delay(100)
+
+        # Capturate the events
+        events = pygame.event.get()
+        for e in events:
+            if e.type == pygame.QUIT:
+                exit()
+        pacman.process_events(events)
